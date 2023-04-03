08848394ca7ab84d6596e2577ce3b207285d5f4f
Byron Miller <byron.miller@slalom.com>
2023-04-03 18:13:17 -0400

    Fix vite config

--- vite.config.ts
+++ vite.config.ts
@@ -5,19 +5,12 @@ import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig(({command, mode}) => {
-  
-  // const env = loadEnv(mode, process.cwd(), '')
-  
-  return {
+export default defineConfig({
     plugins: [react()],
     test: {
     globals: true,
     environment: 'jsdom',
     setupFiles: 'vitest.setup.ts',
     },
-    // define: {
-    //   __SUBMIT_URL__: process.env.REACT_APP_SUBMIT_URL
-    // }
-  }
-})
+    base: '/vite-react-ts'
+  })
