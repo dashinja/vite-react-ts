18c0cf02c5b80cd1f42bb6fbd11f57b9345f70de
Byron Miller <byron.miller@slalom.com>
2023-04-21 19:13:32 -0400

    Kool plugins

--- vite.config.ts
+++ vite.config.ts
@@ -4,11 +4,12 @@
 
 import { defineConfig } from 'vitest/config'
 import {qrcode} from 'vite-plugin-qrcode'
+import { ViteTips } from 'vite-plugin-tips'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react(), qrcode()],
+    plugins: [react(), qrcode(), ViteTips()],
     test: {
     globals: true,
     environment: 'jsdom',
