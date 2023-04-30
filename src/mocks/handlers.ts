6dec0dff44cd0b2ccc9a0ea5dad8676f0956c91c
Byron <byron.miller@gmail.com>
2023-04-30 00:36:06 -0400

    Setup msw for all endpoints | awesome

---
+++ src/mocks/handlers.ts
@@ -0,0 +1,27 @@
+import { rest } from 'msw'
+
+const baseURL = import.meta.env.PROD
+  ? import.meta.env.VITE_SUBMIT_URL
+  : import.meta.env.VITE_SUBMIT_URL_DEV
+
+export const handlers = [
+  rest.post(baseURL, (req, res, ctx) => {
+    const postValue = [1, 2, 3, 4]
+
+    sessionStorage.setItem('listValue', JSON.stringify(postValue))
+
+    return res(ctx.status(200), ctx.json(postValue))
+  }),
+
+  rest.delete(baseURL, (req, res, ctx) => {
+    sessionStorage.removeItem('listValue')
+
+    return res(ctx.status(204))
+  }),
+
+  rest.get(baseURL, (req, res, ctx) => {
+    const listValue = sessionStorage.getItem('listValue')
+
+    return res(ctx.status(200), ctx.json(JSON.parse(listValue || '[]')))
+  }),
+]
