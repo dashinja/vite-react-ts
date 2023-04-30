d782b421f1ad435865d14cd2c05e30136c4d8485
Byron <byron.miller@gmail.com>
2023-04-30 16:01:51 -0400

    msw setup changes

--- vitest.setup.ts
+++ vitest.setup.ts
@@ -1,9 +1,12 @@
 import * as matchers from 'vitest-dom/matchers'
 import { expect } from 'vitest'
-import {server} from './src/mocks/server'
+import { server } from './src/mocks/server'
 
 expect.extend(matchers)
 
 beforeAll(() => server.listen())
-afterEach(() => server.resetHandlers())
+afterEach(() => {
+  server.resetHandlers()
+  sessionStorage.clear()
+})
 afterAll(() => server.close())
